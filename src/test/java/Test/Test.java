f607dab117a08edbb0a36c97dc9eea0ac4030450
an.guoyue <an.guoyue@immomo.com>
2019-10-25 20:30:30 +0800

    update

---
+++ src/test/java/Test/Test.java
@@ -0,0 +1,22 @@
+package Test;
+
+/**
+ * @author SAM{an.guoyue254@gmail.com}
+ * @description Test
+ * @date 2019-10-25 18:39
+ */
+public class Test {
+
+
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+    }
+
+
+}
