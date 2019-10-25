d692b403b92d9a8b1dba65cf9779aa52b03486b2
an.guoyue <an.guoyue@immomo.com>
2019-10-25 11:08:01 +0800

    update

---
+++ src/main/java/com/immomo/test/hacker/Main.java
@@ -0,0 +1,15 @@
+package com.immomo.test.hacker;
+
+/**
+ * @author SAM{an.guoyue254@gmail.com}
+ * @description com.immomo.test.hacker
+ * @date 2019-10-25 10:39
+ */
+public class Main {
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
