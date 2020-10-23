f128938aa0866ffc00b31967b5ad59a92bcd41b6
an.guoyue <an.guoyue@immomo.com>
2020-10-23 18:01:42 +0800

    update

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 cd $(dirname $0)
 mvn -T 2C clean package
 
-rm -rf hacker-cup2020-jar-with-dependencies.jar.jar
-cp target/hacker-cup2020-jar-with-dependencies.jar.jar hacker-cup2020-jar-with-dependencies.jar
+rm -rf hacker-cup2020-jar-with-dependencies.jar
+cp target/hacker-cup2020-jar-with-dependencies.jar hacker-cup2020-jar-with-dependencies.jar
